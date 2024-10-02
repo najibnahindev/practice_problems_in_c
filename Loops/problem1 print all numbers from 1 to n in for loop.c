fb3d5ed83c5ae380602f2073366eabe18e9136e2
Najib Hossain Nahin <najibnahin.dev@gmail.com>
2024-10-02 23:09:31 +0600

    add

--- Loops/problem1 print all numbers from 1 to n in for loop.c
+++ Loops/problem1 print all numbers from 1 to n in for loop.c
@@ -5,7 +5,7 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    for(int i = 1; i <= n; i= i+1)
+    for(int i = 1; i <= n; i= i + 1)
     {
      printf("%d\n", i);
     }
--- Loops/problem2 print all even numbers from 1 to n in for loop.c
+++ Loops/problem2 print all even numbers from 1 to n in for loop.c
@@ -5,7 +5,7 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    for(int i = 1; i <= n; i= i+1)
+    for(int i = 2; i <= n; i= i + 2)
     {
      printf("%d\n", i);
     }
