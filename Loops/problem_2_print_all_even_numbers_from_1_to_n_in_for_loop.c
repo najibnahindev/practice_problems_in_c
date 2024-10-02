7ad749120f8761117f60bdabff89f6d50f8702b2
Najib Hossain Nahin <najibnahin.dev@gmail.com>
2024-10-02 21:41:22 +0600

    Create problem_2_print_all_even_numbers_from_1_to_n_in_for_loop.c

---
+++ Loops/problem_2_print_all_even_numbers_from_1_to_n_in_for_loop.c
@@ -0,0 +1,12 @@
+//Print all numbers from 1 to n in for loop
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i= i+1)
+    {
+     printf("%d\n", i);
+    }
+}
